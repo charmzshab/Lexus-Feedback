51764e95467733c0901380bd7d2f657f0bb613fd
shabix <kmharouna@gmail.com>
2020-06-13 12:41:08 +0300

    added Feedback model

--- app.py
+++ app.py
@@ -14,6 +14,21 @@ else:
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+class Feedback(db.Model):
+    __tablename__='feedback'
+    id = db.Column(db.Integer,primary_key=True)
+    customer = db.Column(db.String(200),unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self,customer,dealer,rating,comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
+        
+
 @app.route('/')
 def index():
     return render_template('index.html')
