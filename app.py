04e209fbe03f30c0043501a42a0ae416b4bd995e
shabix <kmharouna@gmail.com>
2020-06-13 12:15:15 +0300

    added the local postgresql db connection

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+ENV = 'dev' 
+if ENV == 'dev':
+    debug=True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/lexus'
+else:
+    debug=False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -23,4 +31,4 @@ def submit():
 
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
